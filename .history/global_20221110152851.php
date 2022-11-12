c0ec752fa8a5bcf4ee00b489d722c6444995e0c5
Leduc20 <ducnhat31032002@gmail.com>
2022-11-12 14:46:00 +0700

    update name folder

--- .history/global_20221110152851.php
+++ .history/global_20221110152851.php
@@ -8,7 +8,7 @@ function dd(){
 }
 
 
-const BASE_URL = "http://localhost/WEBBANHANG/";
+const BASE_URL = "http://localhost/mvc/";
 const SITE_URL = BASE_URL . "site/index.php";
 const ADMIN_BASE = BASE_URL . 'admin/';
 
