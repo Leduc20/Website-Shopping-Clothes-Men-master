6486ba9fb726d826d5f219ef6bd11743a2ccfe59
Leduc20 <ducnhat31032002@gmail.com>
2022-11-15 15:28:03 +0700

    demo merge

--- index.php
+++ index.php
@@ -3,3 +3,15 @@ require_once './global.php';
 header("location: " . SITE_URL);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>fuck</h1>
+</body>
+</html>
