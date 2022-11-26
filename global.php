93bf36d975e5856d4d602a640a6f4a8f7f9bf7ef
Leduc20 <ducnhat31032002@gmail.com>
2022-11-26 23:40:05 +0700

    update

--- global.php
+++ global.php
@@ -10,3 +10,4 @@ function dd(){
 const BASE_URL = "http://localhost/webbanhang/";
 const SITE_URL = BASE_URL . "site/index.php";
 const ADMIN_BASE = BASE_URL . 'admin/';
+const AUTH_BASE = BASE_URL . 'auth/index.php';
