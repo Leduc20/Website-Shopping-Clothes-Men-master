5a32efbcd39de9f6d8abd58596af15a28cf7d491
hahudev <hahuu02dev@gmail.com>
2022-11-27 12:10:11 +0700

    fixbug

--- global.php
+++ global.php
@@ -6,7 +6,7 @@ function dd(){
     var_dump($args);
     die;
 }
-
+const BASE_URL = "http://localhost:8080/duan1/";
 const SITE_URL = BASE_URL . "site/index.php";
 const ADMIN_BASE = BASE_URL . 'admin/';
 const AUTH_BASE = BASE_URL . 'auth/index.php';
