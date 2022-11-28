d738e5553a528681625ff844bf7859153867a7a7
Quanghuy2310 <huydqph25289@fpt.edu.vn>
2022-11-28 18:41:26 +0700

    update ui page admin

--- admin/danh-muc/add-danh-muc.php
+++ admin/danh-muc/add-danh-muc.php
@@ -5,8 +5,7 @@
         <div class="font-bold px-2 ml-2 mb-1 mt-3"> Tên loại </div>
         <input class="border w-[340px] px-2 ml-4 py-2 mt-1 mb-2  rounded-[4px]" type="text" name="tenloai" id="tenloai"> <br>
         <!-- <button type="submit" class="p-2 rounded border mt-3">Thêm mới</button> -->
-        <!-- <button type="submit" name="themmoi">Thêm mới</button> -->
-        
+        <!-- <button type="submit" name="themmoi">Thêm mới</button> -->  
     </form>
     <div class="mt-4">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" type="submit" name="themmoi">
